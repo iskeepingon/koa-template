85caa5b1230017c33d6ae372cde3d8e7ea5565f4
iskeeping <18279959396@163.com>
2020-07-05 18:56:36 +0800

    init

--- core/middlewares/rule.js
+++ core/middlewares/rule.js
@@ -1,6 +1,9 @@
 module.exports = {
     phone: function (rule, value) {
-        if (rule.required) {
+        if (rule.required == true && rule.allowEmpty != true) {
+            return this.t('should not be empty');
+        }
+        if (rule.required == true) {
             let reg = /^1[3456789]\d{9}$/
             if (!reg.test(value)) {
                 return this.t('should be a phone number')
